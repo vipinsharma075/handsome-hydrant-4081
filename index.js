8742a3ff178b56c8651a80959a82450fe6af18c6
Vipin Sharma <vipsharma54321@gmail.com>
2022-07-21 21:26:06 +0530

    Day3

---
+++ index.js
@@ -0,0 +1,12 @@
+let imagesLink=[
+    "https://akamaividz2.zee5.com/image/upload/w_1013,h_405,c_scale,f_webp,q_auto:eco/resources/0-0-1z5149852/cover/1920x7708760d4e6a1c8429cbfea6f1e4a375f6d25ac3d74d6544bc89704d6475e368c79.jpg",
+    "https://akamaividz2.zee5.com/image/upload/w_1013,h_405,c_scale,f_webp,q_auto:eco/resources/0-0-1z5168144/cover/1920x7709b5906f9ab1347e7915762097af21c2e.jpg",
+    "https://akamaividz2.zee5.com/image/upload/w_1013,h_405,c_scale,f_webp,q_auto:eco/resources/0-0-1z5177236/cover/1920x770cd788cc8f66e4f27accee7da64d1e9cd.jpg",
+    "https://akamaividz2.zee5.com/image/upload/w_1013,h_405,c_scale,f_webp,q_auto:eco/resources/0-6-4z5172356/cover/1920x7705a2073dc89d04da2926dbcd3886785aaa974ce344e704cabb3b0556de80eaa7b.jpg",
+    "https://akamaividz2.zee5.com/image/upload/w_1013,h_405,c_scale,f_webp,q_auto:eco/resources/0-6-366/cover/1920x770e6a8bcf4c3b24c93a74b3dd28f81ead4.jpg"
+]
+let leftbtn=document.querySelector("#left-btn");
+let rightbtn=document.querySelector("#right-btn");
+let crauser=document.querySelector("#crauser");
+
+let imageState=0;
